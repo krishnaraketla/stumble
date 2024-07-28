10c9d1d6c583d9e2c102f0d3557d3bf5acf24cab
raketlak <krishrak97@gmail.com>
2024-07-28 18:02:03 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Sequelize } = require('sequelize');
+const db = require('./models');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Endpoint to create a new user
+app.post('/users', async (req, res) => {
+  try {
+    const user = await db.User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Endpoint to get all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await db.User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+db.sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+});
