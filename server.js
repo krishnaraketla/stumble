eec1bc666dd62bd30b7d56c50803dd14bb0ec85e
raketlak <krishrak97@gmail.com>
2024-07-28 18:15:36 -0400

    test change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const db = require('./models');
 
 const app = express();
-const port = 3000;
+const port = 3000; 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
